6ca573f68cc76751b5da72a982af72d292ec1586
dhileep2000 <dhileepmala@gamil.com>
2024-12-17 00:50:56 +0530

    first commit

--- student/forms.py
+++ student/forms.py
@@ -1,11 +1,15 @@
-from django.forms import ModelForm
+from django import forms
 from .models import *
 
-
-class studentform(ModelForm):
-    
+class studentform(forms.ModelForm):
     class Meta:
         model = studentmodel
         fields = "__all__"
-
-        
+        labels = {'fname': "First Name", 'lname': "Last Name", 'phone': "Phone no"}
+        widgets = {
+            'fname': forms.TextInput(attrs={"class": "form-control-lg"}),
+            'lname': forms.TextInput(attrs={"class": "form-control"}),
+            'email': forms.EmailInput(attrs={"class": "form-control"}),
+            'phone': forms.NumberInput(attrs={"class": "form-control"}),
+            'branch': forms.Select(attrs={"class": "form-control"}),
+        }
